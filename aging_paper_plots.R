f52cb64e96c7bbb273233b0a09ec026f7740ea01
Jenna Krall <jkrall@jhsph.edu>
2014-04-09 11:05:06 -0400

    Add code to work with createdir

--- aging_paper_plots.R
+++ aging_paper_plots.R
@@ -8,7 +8,7 @@ setwd(file.path(home.dir, "plots"))
 
 
 source(file.path(home.dir, "aging_code", "parsing_mplus_files.R"))
-source(file.path(home.dir, "aging_code", "compareres_5jul12.R"))
+source(file.path(home.dir, "aging_code", "aging_plotfun.R"))
 
 
 
@@ -21,14 +21,15 @@ library(lattice)
 
 ###########
 #get output from Mplus
-name1 <- file.path(moddir, "ws_traila_trailb_time_constrain_16apr13.out")
-name2 <- file.path(moddir, "sppb_traila_trailb_time_constrain_16apr13.out")
-name3 <- file.path(moddir, "ws_hvlr_constrain_16apr13.out")
-name4 <- file.path(moddir, "sppb_hvlr_constrain_16apr13.out")
-name5 <- file.path(moddir, "ws_sqhvldel_constrain_16apr13.out")
-name6 <- file.path(moddir, "sppb_sqhvldel_constrain_16apr13.out")
-name7 <-file.path(moddir, "ws_mmse_constrain_17apr13.out")
-name8 <- file.path(moddir, "sppb_mmse_constrain_17apr13.out")
+con <- createDIR(file.path(outfile, "constrain_ageedrace"), "ageedrace_c")
+# name1 <- file.path(moddir, "ws_traila_trailb_time_constrain_16apr13.out")
+# name2 <- file.path(moddir, "sppb_traila_trailb_time_constrain_16apr13.out")
+# name3 <- file.path(moddir, "ws_hvlr_constrain_16apr13.out")
+# name4 <- file.path(moddir, "sppb_hvlr_constrain_16apr13.out")
+# name5 <- file.path(moddir, "ws_sqhvldel_constrain_16apr13.out")
+# name6 <- file.path(moddir, "sppb_sqhvldel_constrain_16apr13.out")
+# name7 <-file.path(moddir, "ws_mmse_constrain_17apr13.out")
+# name8 <- file.path(moddir, "sppb_mmse_constrain_17apr13.out")
 
 	
 	
@@ -50,7 +51,12 @@ pchs <- c(16, 2, 15, 23, 1, 17, 22, 18)
 pchs <- c(16, 17, 15, 18, 1, 2, 22, 23)
 
 
-pdf("Paper_ARPHYS_finalconstrain_25apr13.pdf", height = 7, width = 13)
+
+
+#which file path
+namef <- con
+
+# pdf("Paper_ARPHYS_finalconstrain_25apr13.pdf", height = 7, width = 13)
 
 #for color/ppt
 # cp <- 4
@@ -66,7 +72,10 @@ ca <- 1.5
 cleg <- 1.5
 # par(mfrow = c(1,3), mar = c(6,5,5,0), oma = c(0, 3, 0, 0))
 par(mfrow = c(1,2), mar = c(6,4,5,0), oma = c(0, 6, 0, 0))
-plotsidebyside(name1, name2, "WS", "WS","SP", "SP", c(0,.9), c(1.5,6.5),
+
+
+#TRAIL B
+plotsidebyside(namef[1], namef[5], "WS", "WS","SP", "SP", c(0,.9), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[1], ltys = ltys[1], lwds = 1, col2 = cols[1],
@@ -75,17 +84,17 @@ plotsidebyside(name1, name2, "WS", "WS","SP", "SP", c(0,.9), c(1.5,6.5),
 			# side = 2, line = 0, cex = 3, outer = T)	
 	mtext(expression(atop("Change in baseline sd of x"["t"]) ,"for baseline sd increase in x"["t-1"]), 
 			side = 2, line = -1, cex = 1.5, outer = T)		
-plotsidebyside(name3, name4, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[2], namef[6], "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[2], plot = FALSE, seqs = seq(2.2, 6.2),
 	col2 = cols[2], ltys = ltys[2],cexlab = cl, cexpt = cp, caxis = ca, lwds =1)	
-plotsidebyside(name5, name6, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[3], namef[7], "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[3], plot = FALSE, seqs = seq(2.4, 6.4),
 	col2 = cols[3], ltys = ltys[3], lwds = 1,cexlab = cl, cexpt = cp, caxis = ca)		
-plotsidebyside(name7, name8, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[4], namef[8], "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[4], plot = FALSE, seqs = seq(2.6, 6.6),
@@ -100,24 +109,24 @@ plotsidebyside(name7, name8, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 
 	
 par(mar = c(6,0,5,4))		
-plotsidebyside(name1, name2, "WS", "WS","SP", "SP", c(0,.9), c(1.5,6.5),
+plotsidebyside(namef[1], namef[5], "WS", "WS","SP", "SP", c(0,.9), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[5], plot2 = TRUE, ltys = ltys[4], lwds = 1,
 	col2 = cols[4], axis1 = T,cexlab = cl, cexpt = cp, caxis = ca)	
-plotsidebyside(name3, name4, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[2], namef[6], "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[6], plot = FALSE, seqs = seq(2.2, 6.2),
 	col2 = cols[5], plot2 = TRUE, ltys = ltys[5],
 	cexlab = cl, cexpt = cp, caxis = ca, lwds = 1)	
-plotsidebyside(name5, name6, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[3], namef[7], "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[7], plot = FALSE, seqs = seq(2.4, 6.4),
 	col2 =  cols[6], plot2 = TRUE, ltys = ltys[6], lwds = 1, 
 	cexlab = cl, cexpt = cp, caxis = ca)		
-plotsidebyside(name7, name8, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[4], namef[8], "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 	"a. Walking speed",
 	"b. SPPB", 
 	"previous phys", "phys", pch2 = pchs[8], plot = FALSE, seqs = seq(2.6, 6.6),
@@ -136,7 +145,7 @@ plotsidebyside(name7, name8, "WS", "WS","SP", "SP", c(0.2,.8), c(1.5,6.5),
 # graphics.off()
 
 # plot(1, 1, type = "n", axes = F, xlab = "", ylab = "")
-graphics.off()
+# graphics.off()
 
 
 
@@ -148,7 +157,7 @@ graphics.off()
 # pdf("Paper_ARcog_color_10feb12.pdf",height=7, width=15)
 # pdf("Paper_ARcog_10feb12.pdf",height=7, width=15)
 # pdf("Paper_ARCOG_constrain_16apr13.pdf", height = 7, width = 15)
-pdf("Paper_ARCOG_finalconstrain_25apr13.pdf", height = 9, width = 9)
+# pdf("Paper_ARCOG_finalconstrain_25apr13.pdf", height = 9, width = 9)
 
 # cp <- 4
 # cl <- 3
@@ -157,16 +166,18 @@ pdf("Paper_ARCOG_finalconstrain_25apr13.pdf", height = 9, width = 9)
 # par(mfrow = c(1,3), mar = c(5,4,6,0), oma = c(1, 2, 1, 0))
 # par(mfrow = c(1, 3), mar = c(6, 5, 5, 0), oma = c(0, 3, 0, 0 ))
 par(mfrow = c(2, 2), mar = c(2, 4, 3, 0), oma = c(1, 6, 0, 0 ))
-plotsidebyside(name1, name2,  "TB", "TB","TB", "TB", c(0,1), c(1.5,6.5),
+
+#tb:
+plotsidebyside(namef[5], namef[1],  "TB", "TB","TB", "TB", c(0,1), c(1.5,6.5),
 	"",
 	"b. HVL-imm", 
 	"previous cog", "cog",  pch2 = pchs[1], ltys = ltys[1], lwds = 1, col2 = cols[1],
-	cexlab = cl, cexpt = cp, caxis = ca, axis1 = F)
+	cexlab = cl, cexpt = cp, caxis = ca, axis1 = F, plot2 = T)
 # mtext(paste("AR effects of cognitive function", sep=""), 
 			# side = 2, line = 0, cex = 3, outer = T)		
 				mtext(expression(atop("Change in baseline sd of y"["t"]) ,"for baseline sd increase in y"["t-1"]), 
 			side = 2, line = -1, cex = 1.5, outer = T)		
-plotsidebyside(name1, name2,  "TB", "TB","TB", "TB",c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[1], namef[5],  "TB", "TB","TB", "TB",c(0.2,.8), c(1.5,6.5),
 	"",
 	"b. HVL-imm", 
 	"previous cog", "cog", pch2 = pchs[5], plot = FALSE,plot2 = T, 
@@ -180,12 +191,12 @@ mtext("a. TMT-B", cex = ca)
 		
 
 par(mar = c(2,0,3,4))		
-plotsidebyside(name4, name3,  "HV", "HV","HV", "HV", c(0,1), c(1.5,6.5),
+plotsidebyside(namef[6], namef[2],  "HV", "HV","HV", "HV", c(0,1), c(1.5,6.5),
 	"a. TMT-B",
 	"", 
 	"previous cog", "cog", pch2 = pchs[2], plot2 = TRUE, ltys = ltys[2], lwds = 1,
 	col2 = cols[2],cexlab = cl, cexpt = cp, caxis = ca, axis1 = F)		
-plotsidebyside(name3, name4,  "HV", "HV","HV", "HV", c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[2], namef[6],  "HV", "HV","HV", "HV", c(0.2,.8), c(1.5,6.5),
 	"a. TMT-B",
 	"b. HVL-imm", 
 	"previous cog", "cog",  pch2 = pchs[6], plot = FALSE, seqs = seq(2.2, 6.2),
@@ -198,12 +209,12 @@ legend("bottomleft", col = cols[c(2, 5)], lty = ltys[c(2, 5)],
 mtext("b. HVLT-imm", cex = ca)		
 	
 par(mar = c(5,4,0,0))	
-plotsidebyside(name6, name5, "HV", "HV","HV", "HV", c(0,1), c(1.5,6.5),
+plotsidebyside(namef[7], namef[3], "SH", "SH","SH", "SH", c(0,1), c(1.5,6.5),
 	"a. TMT-B",
 	"", 
 	"previous cog", "cog", pch2 = pchs[3], ltys = ltys[3], lwds = 1,
 	col2 = cols[3],cexlab = cl, cexpt = cp, caxis = ca, plot2 = T, axis2= T)	
-plotsidebyside(name5, name6,  "HV", "HV","HV", "HV",  c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[3], namef[7],  "SH", "SH","SH", "SH",  c(0.2,.8), c(1.5,6.5),
 	"a. TMT-B",
 	"b. HVL-imm", 
 	"previous cog", "cog",  pch2 = pchs[7], plot = FALSE, seqs = seq(2.2, 6.2),
@@ -217,12 +228,12 @@ legend("bottomleft", col = cols[c(3, 6)], lty = ltys[c(3, 6)],
 	mtext(expression(paste("c. HVLT-del"^2)), cex = ca)	
 
 par(mar = c(5,0,0,4))	
-plotsidebyside(name8, name7, "MM", "MM","MM", "MM", c(0,1), c(1.5,6.5),
+plotsidebyside(namef[8], namef[4], "MM", "MM","MM", "MM", c(0,1), c(1.5,6.5),
 	"a. TMT-B",
 	"", 
 	"previous cog", "cog", pch2 = pchs[4], plot2 = TRUE, ltys = ltys[7], lwds = 1,
 	col2 = cols[7],cexlab = cl, cexpt = cp, caxis = ca )	
-plotsidebyside(name7, name8, "MM", "MM","MM", "MM",  c(0.2,.8), c(1.5,6.5),
+plotsidebyside(namef[4], namef[8], "MM", "MM","MM", "MM",  c(0.2,.8), c(1.5,6.5),
 	"a. TMT-B",
 	"b. HVL-imm", 
 	"previous cog", "cog",  pch2 = pchs[8], plot = FALSE, seqs = seq(2.2, 6.2),
@@ -236,7 +247,7 @@ legend("bottomleft", col = cols[c(7, 8)], lty = ltys[c(7, 8)],
 	"(viii): SPPB"), cex = cleg)
 				# c("WS (i, iii, v)",
 	# "SPPB (ii, iv, vi)")
-graphics.off()
+# graphics.off()
 
 
 
