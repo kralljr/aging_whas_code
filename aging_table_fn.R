ce7c4a240ce1554f4df53fd99b481f19319af00e
Jenna Krall <jkrall@jhsph.edu>
2014-04-09 11:03:38 -0400

    Add lines for MMSE, add fn for lag 2 results

--- aging_table_fn.R
+++ aging_table_fn.R
@@ -28,8 +28,8 @@ getests <- function(name, typex, typey, rat = 1) {
 	if(length(whrow) >  0) {
 
 		for(j in 1 : length(whrow)) {
-			print(paste0("X=", typex, ", Y=", typey, 
-				", ci=", out2[whrow[j], ]))
+			# print(paste0("X=", typex, ", Y=", typey, 
+				# ", ci=", out2[whrow[j], ]))
 		}
 	}	
 	
@@ -37,6 +37,59 @@ getests <- function(name, typex, typey, rat = 1) {
 	se <- round(pars$se * rat, 2)
 	out <- data.frame(paste(round(pars$est * rat, 2),
 		 " (", se, ")", sep = ""))
+	
+	list(rev(t(out)), out2)
+}
+
+
+
+getestsLAG <- function(name, typex, typey, rat = 1) {
+	par <- extractModelParameters(name)
+	parcheck <- fixdat(par, std = FALSE)
+	
+	typexs <- substr(parcheck$param, 1, 2)
+	typeys <- substr(parcheck$var, 1, 2)
+	t <- parcheck$param
+	timex <- substr(t, nchar(t), nchar(t))
+	t <- as.character(parcheck$var)
+	timey <- substr(t, nchar(t), nchar(t))
+	onw <- parcheck$onw
+	
+	wh <- which(typexs == typex & 
+		typeys == typey & onw == "ON")
+		
+	timey <- timey[wh]
+	timex <- timex[wh]
+	
+	
+	pars <- parcheck[wh, ]
+	parmat <- matrix(nrow = nrow(pars), ncol = 3)
+	parmat <- data.frame(parmat)
+	# parmat[,2] <- paste(pars$param, pars$var)
+	parmat[,1] <- round(pars$est, 2)
+	rownames(parmat) <- paste0(typexs[wh],timex, typeys[wh], timey)
+	
+	#CONFIDENCE INTERVAL
+	rnd <- 10
+	lb <- round(pars$est - 2 * pars$se, 10) * rat
+	ub <- round(pars$est + 2 * pars$se, 10) * rat
+	out2 <- data.frame(paste(round(pars$est * rat, 10 ),
+		" (", lb, ", ", ub, ")", sep = ""))
+		
+	whrow <- which(lb >= 0 | ub <= 0)
+	if(length(whrow) >  0) {
+
+		for(j in 1 : length(whrow)) {
+			# print(paste0("X=", typex, ", Y=", typey, 
+				# ", ci=", out2[whrow[j], ]))
+		}
+	}	
+	
+	# #SE ONLY
+	se <- round(pars$se * rat, 2)
+	out <- data.frame(paste(round(pars$est * rat, 2),
+		 " (", se, ")", sep = ""))
+	rownames(out2) <- rownames(parmat)
 	
 	list(rev(t(out)), out2)
 }
@@ -54,23 +107,25 @@ incbreaks <- function(out1f) {
 
 
 makedfP <- function (name1, x1, y1, name2, x2, y2,
-	name3, x3, y3, type = "no", rats = c(1, 1, 1)) {
+	name3, x3, y3, name4, x4, y4, type = "no", rats = c(1, 1, 1, 1)) {
 		
 		
 	out1a <- getests(name1, x1, y1, rat = rats[1])
 	out1b <-  getests(name2, x2, y2, rat = rats[2])
 	out1c <- getests(name3, x3, y3, rat = rats[3])
+	out1d <- getests(name4, x4, y4, rat = rats[4])
 	
 	if(type == "SP") {
 		out1 <- data.frame(rbind(incbreaks(out1a[[1]]), 
-			incbreaks(out1b[[1]]), incbreaks(out1c[[1]])))
+			incbreaks(out1b[[1]]), incbreaks(out1c[[1]]), 
+			incbreaks(out1d[[1]])))
 	}else{
 		out1 <- data.frame(rbind(out1a[[1]], 
-			out1b[[1]], out1c[[1]]))
+			out1b[[1]], out1c[[1]], out1d[[1]]))
 		}
 	rownames(out1) <- NULL
 	
-	list(out1, list(out1a[[2]], out1b[[2]], out1c[[2]]))
+	list(out1, list(out1a[[2]], out1b[[2]], out1c[[2]], out1d[[2]]))
 }
 
 
@@ -78,30 +133,36 @@ makedfP <- function (name1, x1, y1, name2, x2, y2,
 # listnames[[2]] is x1 y2
 # listnames[[3]] is x2 y1
 # listnames[[4]] is x2 y2
-tabfunP <- function(listnames, x1, x2, x3, y1, y2, rats) {
+tabfunP <- function(listnames, x1, x2, x3, x4, y1, y2, rats) {
 	
 	
 	#ALL CL for WS
-	#tb -> ws, hvlr -> ws, sqhvldel -> ws
-	out1 <- makedfP(listnames[[1]], x1, y1, 
-		listnames[[3]], x2, y1,
-		listnames[[5]], x3, y1, rats = rats[1:3])
+	#tb -> ws, hvlr -> ws, sqhvldel -> ws, mmse -> ws
+	out1 <- makedfP(listnames[1], x1, y1, 
+		listnames[2], x2, y1,
+		listnames[3], x3, y1, 
+		listnames[4], x4, y1,
+		rats = rats[1:4])
 	#ALL CL for SPPB
 	#tb -> sppb, hvlr -> sppb, sqhvldel -> sppb
-	out2 <- makedfP(listnames[[2]], x1, y2, 
-		listnames[[4]], x2, y2,
-		listnames[[6]], x3, y2, type = "SP", rats = rats[4:6])
+	out2 <- makedfP(listnames[5], x1, y2, 
+		listnames[6], x2, y2,
+		listnames[7], x3, y2, 
+		listnames[8], x4, y2, 
+		type = "SP", 
+		rats = rats[5:8])
 	#ALL AR for WS
 	#tb -> ws, hvlr -> ws, sqhvldel -> ws
-	out3 <- makedfP(listnames[[1]], y1, y1, 
-		listnames[[3]], y1, y1,
-		listnames[[5]], y1, y1)
+	out3 <- makedfP(listnames[1], y1, y1, 
+		listnames[2], y1, y1,
+		listnames[3], y1, y1, 
+		listnames[4], y1, y1)
 	#ALL AR for SPPB
 	#tb -> sppb, hvlr -> sppb, sqhvldel -> sppb
-	out4 <- makedfP(listnames[[2]], y2, y2, 
-		listnames[[4]], y2, y2,
-		listnames[[6]], y2, y2, type = "SP")
-
+	out4 <- makedfP(listnames[5], y2, y2, 
+		listnames[6], y2, y2,
+		listnames[7], y2, y2, 
+		listnames[8], y2, y2, type = "SP")
 
 	list(data.frame(rbind(out1[[1]], out2[[1]], out3[[1]], out4[[1]])),
 		list(out1[[2]], out2[[2]], out3[[2]], out4[[2]]))
@@ -116,7 +177,7 @@ tabfunP <- function(listnames, x1, x2, x3, y1, y2, rats) {
 
 #####MAKE COGNITIVE TABLE
 
-makedf <- function (name1, x1, y1, name2, x2, y2, type = "no", rats = c(1,1)) {
+makedfC <- function (name1, x1, y1, name2, x2, y2, type = "no", rats = c(1,1)) {
 	out1a <- getests(name1, x1, y1, rat = rats[1])
 	out1b2 <-  getests(name2, x2, y2, rat = rats[2])
 	
@@ -136,34 +197,37 @@ makedf <- function (name1, x1, y1, name2, x2, y2, type = "no", rats = c(1,1)) {
 # listnames[[2]] is x1 y2
 # listnames[[3]] is x2 y1
 # listnames[[4]] is x2 y2
-tabfunC <- function(listnames, x1, x2, y1, y2, y3, rats) {
+tabfunC <- function(listnames, x1, x2, y1, y2, y3, y4, rats) {
 	
 	#all cl for tb
 	#ws -> tb, sppb-> tb
-	out1 <- makedfC(listnames[[1]], x1, y1, 
-		listnames[[3]], x2, y1, rats = rats[1:2])
+	out1 <- makedfC(listnames[1], x1, y1, 
+		listnames[5], x2, y1, rats = rats[1:2])
 	#all cl for hvlimm
 	#ws -> hvlr, sppb-> hvlr
-	out2 <- makedfC(listnames[[2]], x1, y2, 
-		listnames[[4]], x2, y2, rats = rats[3:4])	
+	out2 <- makedfC(listnames[2], x1, y2, 
+		listnames[6], x2, y2, rats = rats[3:4])	
 	#all cl for hvldel
 	#ws -> hvldel, sppb-> hvldel
-	out3 <- makedfC(listnames[[5]], x1, y3, 
-		listnames[[6]], x2, y3, rats = rats[5:6])	
+	out3 <- makedfC(listnames[3], x1, y3, 
+		listnames[7], x2, y3, rats = rats[5:6])	
+	out4 <- makedfC(listnames[4], x1, y4, 
+		listnames[8], x2, y4, rats = rats[5:6])	
 		
 	#allAR for tb	
 	#ws -> tb, sppb-> tb
-	out4 <- makedfC(listnames[[1]], y1, y1, 
-		listnames[[3]], y1, y1)
+	out5 <- makedfC(listnames[1], y1, y1, 
+		listnames[5], y1, y1)
 	#all AR for hvlimm
 	#ws -> hvlr, sppb-> hvlr
-	out5 <- makedfC(listnames[[2]], y2, y2, 
-		listnames[[4]], y2, y2)	
+	out6 <- makedfC(listnames[2], y2, y2, 
+		listnames[6], y2, y2)	
 	#all AR for hvldel
 	#ws -> hvldel, sppb-> hvldel
-	out6 <- makedfC(listnames[[5]], y3, y3, 
-		listnames[[6]], y3, y3)	
-
+	out7 <- makedfC(listnames[3], y3, y3, 
+		listnames[7], y3, y3)	
+	out7 <- makedfC(listnames[4], y4, y4, 
+		listnames[8], y4, y4)	
 
 	list(data.frame(rbind(out1[[1]], out2[[1]], out3[[1]], 
 		out4[[1]], out5[[1]], out6[[1]])),
@@ -176,10 +240,9 @@ tabfunC <- function(listnames, x1, x2, y1, y2, y3, rats) {
 
 
 
-getLRTpval <- function(name1, fp2, name2, type = "no") {
+getLRTpval <- function(name1, name2, type = "no") {
 	par1 <- extractModelSummaries(name1)
-	#par1 <- readModels(name1)
-	name2 <- file.path(fp2, name2)
+
 	par2 <- extractModelSummaries(name2)
 	#par2 <- readModels(name2)
 	#compareModels(par1, par2, show = "summaries")
